52c9325da0074117d237619ba132b7e54ad0a4b9
ung thi hoang diep <you@example.com>
2019-11-20 19:59:58 +0700

    fist

---
+++ HTML-CSS/Bai1.js
@@ -0,0 +1,27 @@
+//    document.getElementById("test").innerHTML = "hello everybody";
+//    function btnon(){
+//        document.getElementById("light").src='images/images.jpg';
+//    }
+//    function btnoff(){
+//        document.getElementById("light").src='images/sang.png';
+//    }
+// function light(a){
+//     if(a==0){
+//         document.getElementById("light").src='images/images.jpg';
+//     }else{
+//         document.getElementById("light").src='images/sang.png';
+//     }
+// }
+function threelight(a){
+    if(a==1){
+    document.getElementById("light").src='images/red.jpg';
+    }
+    else if(a==2){
+        document.getElementById("light").src='images/blue.jpg';
+    }
+    else{
+        document.getElementById("light").src='images/orance.jpg';
+    }
+}
+    
+
